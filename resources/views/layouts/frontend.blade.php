d71260732f22117fc75e089c3c3f43b42f1d579f
yakuparas <yakuparas28@gmail.com>
2022-06-29 16:15:58 +0300

    Update frontend.blade.php

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -53,30 +53,7 @@
 
 <!-- footer -->
 <footer class="footer-light">
-    <div class="light-layout">
-        <div class="container">
-            <section class="small-section border-section border-top-0">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="subscribe">
-                            <div>
-                                <h4>KNOW IT ALL FIRST!</h4>
-                                <p>Never Miss Anything From Multikart By Signing Up To Our Newsletter.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <form action="https://pixelstrap.us19.list-manage.com/subscribe/post?u=5a128856334b598b395f1fc9b&amp;id=082f74cbda" class="form-inline subscribe-form auth-form needs-validation" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank">
-                            <div class="form-group mx-sm-3">
-                                <input type="text" class="form-control" name="EMAIL" id="mce-EMAIL" placeholder="Enter your email" required="required">
-                            </div>
-                            <button type="submit" class="btn btn-solid" id="mc-submit">subscribe</button>
-                        </form>
-                    </div>
-                </div>
-            </section>
-        </div>
-    </div>
+  
     <section class="section-b-space light-layout">
         <div class="container">
             <div class="row footer-theme partition-f">
